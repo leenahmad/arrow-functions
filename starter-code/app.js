7d9c142afd0d7109afa570d3140703f569c3a87d
leenahmad <leenkaraja24@gmail.com>
2021-09-11 21:41:27 +0300

    step11 and question done

--- starter-code/app.js
+++ starter-code/app.js
@@ -189,16 +189,21 @@ Student.prototype.scope = function() {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(joe.scope());
+console.log(joe.scope());
 
 Student.prototype.scopeArrow = () => console.log(this);
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(joe.scopeArrow());
+console.log(joe.scopeArrow());
+
 
 // TODO: Write a COMMENT below to answer the following questions.
 // 1. What is "this" when joe.scope() is invoked?
 //
+//represented the object that called the function, which could be the window, the document, a button or whatever.
+
 // 2. What is "this" when joe.scopeArrow() is invoked?
-//
+//represents the object that defined the arrow function.
+
 // 3. Explain why "this" is different when an arrow function is used.
+// In classic function expressions, the this keyword is bound to different values based on the context in which the function is called. Whereas arrow functions use the value of this in their lexical scope.
